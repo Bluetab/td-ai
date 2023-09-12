d5e265a1a02f651b045ce10b5285a6055cd29299
Guillermo Rodriguez <guillermo.r.navarrete@bluetab.net>
2023-09-12 15:43:42 +0200

    fix: config hostname file

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :td_ai, TdAi.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "postgres",
   database: "td_ai_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
--- config/test.exs
+++ config/test.exs
@@ -8,7 +8,7 @@ import Config
 config :td_ai, TdAi.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "postgres",
   database: "td_ai_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
